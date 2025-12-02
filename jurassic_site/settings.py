2f67dd6b0e1086f343844a8505ac9fafd2c1c088
Ivann <212ivann.rambaud@gmail.com>
2025-12-02 11:56:01 +0100

    add whitenoise

--- jurassic_site/settings.py
+++ jurassic_site/settings.py
@@ -53,6 +53,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+
 ]
 
 ROOT_URLCONF = 'jurassic_site.urls'
@@ -133,6 +135,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 import os
 
 MEDIA_URL = '/media/'
